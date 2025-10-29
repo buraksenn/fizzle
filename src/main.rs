4fb8c79f029931774cce4da7926166b26c25a4bb
buraksenn <buraksenb@gmail.com>
2025-10-29 22:59:00 +0300

    chore: move token to its own package

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,5 @@
+use fizzle::token::Token;
 use std::{iter::Peekable, str::Chars};
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum Token {
-    Illegal,
-    Eof,
-    Ident(String),
-    Int(i32),
-
-    Assign,
-    Plus,
-    Minus,
-    Bang,
-    Asterisk,
-    Slash,
-
-    Lt,
-    Gt,
-
-    Comma,
-    Semicolon,
-
-    Lparen,
-    Rparen,
-    Lbrace,
-    Rbrace,
-
-    Function,
-    Let,
-    True,
-    False,
-    If,
-    Else,
-    Return,
-}
-
-impl Token {
-    pub fn lookup_ident(ident: String) -> Token {
-        match ident.as_str() {
-            "let" => Token::Let,
-            "fn" => Token::Function,
-            "true" => Token::True,
-            "false" => Token::False,
-            "if" => Token::If,
-            "else" => Token::Else,
-            "return" => Token::Return,
-            _ => Token::Ident(ident),
-        }
-    }
-}
 
 struct Lexer<'a> {
     chars: Peekable<Chars<'a>>,
---
+++ src/token/mod.rs
@@ -0,0 +1,48 @@
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Token {
+    Illegal,
+    Eof,
+    Ident(String),
+    Int(i32),
+
+    Assign,
+    Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    Slash,
+
+    Lt,
+    Gt,
+
+    Comma,
+    Semicolon,
+
+    Lparen,
+    Rparen,
+    Lbrace,
+    Rbrace,
+
+    Function,
+    Let,
+    True,
+    False,
+    If,
+    Else,
+    Return,
+}
+
+impl Token {
+    pub fn lookup_ident(ident: String) -> Token {
+        match ident.as_str() {
+            "let" => Token::Let,
+            "fn" => Token::Function,
+            "true" => Token::True,
+            "false" => Token::False,
+            "if" => Token::If,
+            "else" => Token::Else,
+            "return" => Token::Return,
+            _ => Token::Ident(ident),
+        }
+    }
+}
