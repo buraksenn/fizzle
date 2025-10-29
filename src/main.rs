342427a5b073b8a69a4b184bc1d74c8c9a52c6fc
buraksenn <buraksenb@gmail.com>
2025-10-29 23:19:17 +0300

    feat: adjust main for repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-pub fn main() {
-    println!("Hello World from Fizzle!")
+use std::io;
+
+use fizzle::repl;
+
+fn main() {
+    println!("Hello, this is the Monkey programming language!");
+    println!("Feel free to type in commands");
+
+    repl::start(io::stdin().lock(), io::stdout());
 }
