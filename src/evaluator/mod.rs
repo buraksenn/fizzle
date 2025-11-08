5c289fbeaa699bd2c7b95c8a958e964757b3f55a
buraksenn <buraksenb@gmail.com>
2025-11-09 00:49:25 +0300

    feat: bang prefix expression support

--- src/evaluator/mod.rs
+++ src/evaluator/mod.rs
@@ -1,11 +1,14 @@
+use anyhow::anyhow;
+
 use crate::{
     ast::{Expression, Node, Program, Statement},
     object::Object,
+    token::{self, Token},
 };
 
-type EvaluatorResult<T> = Result<T, anyhow::Error>;
+type EvaluatorResult = Result<Object, anyhow::Error>;
 
-pub fn evaluate(n: Node) -> EvaluatorResult<Object> {
+pub fn evaluate(n: Node) -> EvaluatorResult {
     match n {
         Node::Expression(exp) => evaluate_expression(&exp),
         Node::Program(p) => evaluate_program(&p),
@@ -13,7 +16,7 @@ pub fn evaluate(n: Node) -> EvaluatorResult<Object> {
     }
 }
 
-fn evaluate_program(prog: &Program) -> EvaluatorResult<Object> {
+fn evaluate_program(prog: &Program) -> EvaluatorResult {
     let mut result = Object::Null;
     for st in prog.statements.iter() {
         result = evaluate_statement(st)?;
@@ -22,18 +25,39 @@ fn evaluate_program(prog: &Program) -> EvaluatorResult<Object> {
     Ok(result)
 }
 
-fn evaluate_statement(st: &Statement) -> EvaluatorResult<Object> {
+fn evaluate_statement(st: &Statement) -> EvaluatorResult {
     match st {
         Statement::Expression { value } => evaluate_expression(value),
         _ => todo!(),
     }
 }
 
-fn evaluate_expression(exp: &Expression) -> EvaluatorResult<Object> {
+fn evaluate_expression(exp: &Expression) -> EvaluatorResult {
     match exp {
         Expression::IntegerLiteral(i) => Ok(Object::Integer(*i)),
         Expression::Boolean(b) => Ok(Object::Boolean(*b)),
+        Expression::Prefix { operator, operand } => {
+            let right = evaluate_expression(operand)?;
+            evaluate_prefix_expression(operator, right)
+        }
         _ => todo!(),
+    }
+}
+
+fn evaluate_prefix_expression(operator: &Token, operand: Object) -> EvaluatorResult {
+    match operator {
+        Token::Bang => evaluate_bang_expression(operand),
+        tok => Err(anyhow!(
+            "expected ! but got unsupported {} token while evaluation prefix expression",
+            tok
+        )),
+    }
+}
+
+fn evaluate_bang_expression(operand: Object) -> EvaluatorResult {
+    match operand {
+        Object::Boolean(false) | Object::Null => Ok(Object::Boolean(true)),
+        _ => Ok(Object::Boolean(false)),
     }
 }
 
@@ -85,6 +109,45 @@ mod test {
         for case in tests.iter() {
             let obj = eval(case.input);
             assert_boolean_object(obj, case.expected);
+        }
+    }
+
+    #[test]
+    fn test_bang_operator() {
+        struct Test<'a> {
+            input: &'a str,
+            expected: bool,
+        }
+        let tests = vec![
+            Test {
+                input: "!true",
+                expected: false,
+            },
+            Test {
+                input: "!false",
+                expected: true,
+            },
+            Test {
+                input: "!5",
+                expected: false,
+            },
+            Test {
+                input: "!!true",
+                expected: true,
+            },
+            Test {
+                input: "!!false",
+                expected: false,
+            },
+            Test {
+                input: "!!5",
+                expected: true,
+            },
+        ];
+
+        for t in tests {
+            let evaluated = eval(t.input);
+            assert_boolean_object(evaluated, t.expected);
         }
     }
 
